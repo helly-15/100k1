e794e428b7bb2a381105c40818e717ceda3cdf93
Lenocka <lvutina.en@gmail.com>
2021-10-22 01:18:34 +0300

    feat: add scores to replies

--- src/components/question-list/RepliesList.js
+++ src/components/question-list/RepliesList.js
@@ -3,21 +3,32 @@ import './RepliesList.scss';
 const classnameRoot = 'replies-list';
 
 export const RepliesList = (props) => {
+
     return <div className={ classnameRoot }>
         { props.repliesForRound.map((reply, index) => {
+            const isReplyGuessed = props.guessedReplies.includes(index);
             return <button className={ classnameRoot + '__reply' }
                            onClick={ (e) => {
                                let replyAnswerSound = new Audio("/line_open.mp3");
                                replyAnswerSound.play();
                                e.target.style.backgroundColor = 'black';
-                               e.target.firstChild.style.backgroundColor = 'black';
+                              // e.target.firstChild.style.backgroundColor = 'black';
                                props.setRepliesForRoundOpened(index)
                            }
                            }
                            key={ 'reply-button' + index }
             >
-                <div className={ classnameRoot + '__reply_answer' }>
-                    { reply }
+                <div className={ `${classnameRoot}__reply_answer ${isReplyGuessed?classnameRoot+'__reply_answer_answered':''}`}>
+                    { isReplyGuessed?
+                        <div className={ classnameRoot + '__reply_wrapper' }>
+                            <span className={ classnameRoot + '__reply_text' }>
+                             {reply}
+                            </span>
+                            <span className={ classnameRoot + '__reply_text' }>
+                             { props.repliesScores[index]}
+                            </span>
+                        </div>
+                        : reply }
                 </div>
             </button>
         }) }
--- src/components/questions-board/QuestionsBoard.js
+++ src/components/questions-board/QuestionsBoard.js
@@ -6,14 +6,20 @@ import {useState} from "react";
 const classnameRoot = 'questions-board';
 
 export const QuestionsBoard = () => {
-    const correctReplies = ['Ok', 'Привет', 'Поцелуй', ' Воинское приветствие', 'Рожки', 'Рукопожатие']
+    const correctReplies = ['Ok', 'Привет', 'Поцелуй', ' Воинское приветствие', 'Рожки', 'Рукопожатие'];
+    const repliesScores = ['27', '19', '16', ' 13', '10', '5'];
+    const [guessedReplies, setGuessedReplies] = useState([]);
     const [repliesForRound, setRepliesForRound] = useState(['1', '2', '3', '4', '5', '6']);
     const setRepliesForRoundOpened = (index) => {
         setRepliesForRound([
             ...repliesForRound.slice(0, index),
             correctReplies[index],
             ...repliesForRound.slice(index + 1)
-        ])
+        ]);
+        setGuessedReplies([
+            ...guessedReplies,
+            index
+        ]);
     };
 
     return <div className={ classnameRoot }>
@@ -21,6 +27,8 @@ export const QuestionsBoard = () => {
         <RepliesList repliesForRound={ repliesForRound }
                      setRepliesForRoundOpened={ setRepliesForRoundOpened }
                      correctReplies={ correctReplies }
+                     guessedReplies = {guessedReplies}
+                     repliesScores = {repliesScores }
         />
         <MistakesCounter/>
     </div>
